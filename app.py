5d79143c1066d1737135bdf8497dc39aa98d9fd1
tmdghlrla <tmdghlrla@google.com>
2023-11-27 13:08:08 +0900

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def main() :
         "container": {"padding": "4!important", "background-color": "#fafafa"},
         "icon": {"color": "black", "font-size": "25px"},
         "nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px", "--hover-color": "#fafafa"},
-        "nav-link-selected": {"background-color": "#08c7b4"},
+        "nav-link-selected": {"background-color": "#d3d3d3"},
     }
     )
 
